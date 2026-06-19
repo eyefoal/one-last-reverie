796782c89532ed39e6194aaad5edfaa8a13a343c
eyefoal <jollyjollyjolyne@gmail.com>
2026-06-18 20:11:07 -0400

    cat doors

--- main/main.gd
+++ main/main.gd
@@ -2,3 +2,10 @@ extends Node
 
 var global_state : String = "Act -1"
 var gb_cat_state : String = "unknown"
+
+func _ready() -> void:
+	dev_print()
+	
+func dev_print():
+	print(global_state)
+	print(gb_cat_state)
---
+++ systems/components/doors/catdoor/catdoor.gd
@@ -0,0 +1,28 @@
+extends Area2D
+class_name CatDoor
+
+@export var to_level : PackedScene
+var body_to_transport : CharacterBody2D
+var can_warp : bool = false
+
+func _process(_delta: float) -> void:
+	if can_warp and body_to_transport and Input.is_action_just_pressed("a"):
+		warp()
+	
+	
+func warp() -> void:
+	#await FadeTransition.fade(1.0, 1.0).finished
+	get_tree().call_deferred("change_scene_to_packed", to_level)
+	#await FadeTransition.fade(0.0, 1.0).finished
+		
+		
+func _on_body_entered(body: Node2D) -> void:
+	if body is CharacterBody2D or Lofi and is_in_group("Player"):
+		body_to_transport = body
+		can_warp = true
+		print(body_to_transport)
+
+
+@warning_ignore("unused_parameter")
+func _on_body_exited(body: Node2D) -> void:
+	can_warp = false
