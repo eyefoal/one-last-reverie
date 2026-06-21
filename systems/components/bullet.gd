6e52b462152ba7092fe6b2146866ada4d27c9eec
eyefoal <jollyjollyjolyne@gmail.com>
2026-06-21 13:40:21 -0400

    maxinedlalogue

--- systems/components/bullet.gd
+++ systems/components/bullet.gd
@@ -1,8 +1,8 @@
 extends Area2D
 class_name Bullet
 
+@onready var camera := get_tree().get_first_node_in_group("Camera")
 @export var timer : Timer 
-
 @export_category("Bullet Stats")
 @export var knockback_force : float = 42.0
 @export var power : float = 1.0
@@ -10,9 +10,6 @@ class_name Bullet
 @export var lifetime : float
 
 var direction : Vector2
-
-
-
 
 func setup(pos: Vector2, dir: Vector2):
 	position = pos + 16 * dir
@@ -22,19 +19,19 @@ func _physics_process(delta: float) -> void:
 	
 	global_position += direction * speed * delta
 
-
 func _on_body_entered(body: Node2D) -> void:
 	if body is TileMapLayer:
 		queue_free()
 	
 	if body is BreakableBlock:
 		body.queue_free()
+		if camera:
+			camera.shake()
 		queue_free()
 	
 func _on_visible_on_screen_notifier_2d_screen_exited() -> void:
 	queue_free()
 	print("dead")
-
 
 func _on_timer_timeout() -> void:
 	queue_free()
